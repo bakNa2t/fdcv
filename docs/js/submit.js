de69a72de36e40825fec47d0eac370fbdc4f96a2
bakNa2t <ok2.lock@gmail.com>
2023-11-14 13:56:20 +0300

    Update submit.js

--- docs/js/submit.js
+++ docs/js/submit.js
@@ -1,29 +1,30 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbyNWN9bwKI2NtOM4g1goKl1e6Kt3-rHnx1N2PYXY-eWmh7RmeBgQsuwsZGrK8aJdMVbhg/exec';
-const form = document.forms['submit-to-google-sheet'];
-const msg = document.querySelector('#msg');
-const subBtn = document.querySelector('.sub-btn');
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbyNWN9bwKI2NtOM4g1goKl1e6Kt3-rHnx1N2PYXY-eWmh7RmeBgQsuwsZGrK8aJdMVbhg/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.querySelector("#msg");
+const subBtn = document.querySelector(".sub-btn");
 
-form.addEventListener('submit', e => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then(() => {
       msg.innerHTML = "Message sent successfully!";
       msg.style.backgroundColor = "var(--color-secondary)";
       msg.style.borderRadius = ".8rem";
-      msg.style.padding = ".8rem 1.5rem"; 
-      setTimeout(function(){
+      msg.style.padding = ".8rem 1.5rem";
+      setTimeout(function () {
         msg.innerHTML = "";
         msg.removeAttribute("style");
       }, 3000);
       form.reset();
     })
-    .catch(error => alert.error('Message not sent!', error.message));
+    .catch((error) => alert.error("Message not sent!", error.message));
 });
 
 //Show alert message after submit
-subBtn.addEventListener('click', () => {
-  const inputName = document.getElementById('input-name');
+subBtn.addEventListener("click", () => {
+  const inputName = document.getElementById("input-name");
   if (inputName.value !== "") {
-    alert("Message sent successfully!")
+    alert("Message sent successfully!");
   }
 });
